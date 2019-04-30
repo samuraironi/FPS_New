db2cd462df6ecc6cb03cce95a8c31ae148f69bdc
danny <daniil.tcurkan@mail.ru>
2019-04-30 21:24:58 +0300

    refactored inheritance

--- Source/FPS/ChooseNextWaypoint.cpp
+++ Source/FPS/ChooseNextWaypoint.cpp
@@ -1,27 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-#include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
 	//UE_LOG(LogTemp, Warning, TEXT("suceeded"));
+	//Get patrol route
+	auto controlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto patrolRoute = controlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(patrolRoute)) { return EBTNodeResult::Failed; }
 
-	//Get patrol points
-	auto aiController = OwnerComp.GetAIOwner();
-	auto controlledPawn = aiController->GetPawn();
-	auto patrollingGuard = Cast<APatrollingGuard>(controlledPawn);
-	auto patrollinPoints = patrollingGuard->PatrollPointsCPP;
+	auto patrollPoints = patrolRoute->GetPatrolPoints();
+	if (patrollPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patroll points"));
+		return EBTNodeResult::Failed;
+	}
 
 	//Set next way point
 	auto blackBoardComponet = OwnerComp.GetBlackboardComponent();
 	auto index = blackBoardComponet->GetValueAsInt(FName(IndexKey.SelectedKeyName));
-	blackBoardComponet->SetValueAsObject(WayPointKey.SelectedKeyName, patrollinPoints[index]);
+	blackBoardComponet->SetValueAsObject(WayPointKey.SelectedKeyName, patrollPoints[index]);
 
 	//Cycle the index
-	auto nextIndex = (index + 1) % patrollinPoints.Num();
+	auto nextIndex = (index + 1) % patrollPoints.Num();
 	blackBoardComponet->SetValueAsInt(IndexKey.SelectedKeyName, nextIndex);
 
 	return EBTNodeResult::Succeeded;
